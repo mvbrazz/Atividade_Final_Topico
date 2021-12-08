923b9053b8f5adc807543bbe7502b9e2c7fc296e
Marcus Vinicus <mvbrazz@hotmail.com>
2021-12-08 18:29:25 -0300

    Commit #2

--- server.js
+++ server.js
@@ -7,10 +7,15 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+async function getInscricao(){
+    await storage.init();
+    const email = await storage.getItem('Email');    
+    return email;
+}
+
 async function getNoticias(){
     await storage.init();
-    const noticias = await storage.getItem('Noticias');
-    //console.log(noticias);
+    const noticias = await storage.getItem('Noticias');    
     return noticias;
 }
 
@@ -21,21 +26,39 @@ async function postNoticias(noticia){
     const Noticias = await storage.getItem('Noticias');
 
     if(Noticias == null){
-        await storage.setItem('Noticias',[{ID:0,Titulo:'O inicio',resumo: 'Iniciado',url: 'www.google.com.br'}]);
+        console.log("1");
+        await storage.setItem('Noticias',[{ID:0,Titulo: noticia.Titulo,resumo: noticia.resumo,url: noticia.url}]);
     }
     else{
-        
+        console.log("2");
         Noticias.push({ID:Noticias.length,Titulo: noticia.Titulo,resumo: noticia.resumo,url: noticia.url});
         await storage.updateItem('Noticias',Noticias);
-        
     }
 
     console.log('Noticia Salva');
     
 }
 
+async function postInscricao(e_mail){
+    await storage.init();
+    
+    const Email = await storage.getItem('Email');
+    if(Email == null){
+        console.log("1");
+        await storage.setItem('Email',[{Email:e_mail.Email}]);  
+    }
+    else{
+        console.log("2");
+        Email.push({Email:e_mail.Email});
+        await storage.updateItem('Email',Email);
+    }
+      
+
+    console.log('Email salvo');
+}
+
 app.get('/noticias', (req, res) => {
-    var Note;
+
     const noticias = getNoticias();
     noticias.then(v => {
         res.send(v); 
@@ -43,11 +66,42 @@ app.get('/noticias', (req, res) => {
  
 });
 
+app.get('/noticias/:ID', (req, res) => {
+    const IDs = parseInt(req.params.ID);
+    var aux;
+    if (isNaN(IDs)) {
+        res.status(500).send('Non integer');
+        return;
+    }
+    else{
+        const noticias = getNoticias();
+
+        noticias.then(v => {
+            aux = v.find(b => b.ID == IDs); 
+            if(!aux){
+                res.status(500).send('Non integer');
+                return;
+            }
+            else{
+                res.send(aux);
+            }
+            
+        });
+    }
+});
+
 app.post('/noticias', (req, res) => {
     const noticia = req.body; 
     postNoticias(noticia);
     res.send('Noticia Adicionada');
     return noticia;
+});
+
+app.post('/inscricao', (req, res) => {
+    const email = req.body; 
+    postInscricao(email);
+    res.send('Email Adicionado');
+    return email;
 });
 
 app.listen(3000, () => {
